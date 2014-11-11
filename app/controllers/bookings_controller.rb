0052dbee9c8f3be71b34c9a1326464d13576b7a2
Luiggi Franceschi <luiggi_ff@yahoo.com.ar>
2014-11-11 18:15:28 -0300

    crear reservas agregado

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,10 +5,9 @@ class BookingsController < ApplicationController
   
 
   def index
-    uri = "#{API_BASE_URL}/resources/#{params[:id]}/bookings?status=all" # specifying json format in the URl
+    uri = "#{API_BASE_URL}/resources/#{params[:r_id]}/bookings?status=all" # specifying json format in the URl
     rest_resource = RestClient::Resource.new(uri) # It will create
     #new rest-client resource so that we can call different methods of it
-    p params
     bookings = rest_resource.get # will get back you all the detail in json format, but it
     #will we wraped as string, so we will parse it in the next step.
     @bookings = JSON.parse(bookings, :symbolize_names => true)[:bookings] # we will convert the return
@@ -16,6 +15,24 @@ class BookingsController < ApplicationController
       url = b[:links].first[:uri]
       id = URI(url).path.split('/').last
       b[:id] = id
+    end
+  end
+
+  def new 
+
+  end
+
+  def create
+    uri = "#{API_BASE_URL}/resources/#{params[:r_id]}/bookings"
+    payload = params#.to_json # converting the params to json
+    rest_resource = RestClient::Resource.new(uri)
+    begin
+      rest_resource.post payload , :content_type => 'text/plain'
+      flash[:notice] = "Booking Saved successfully"
+      redirect_to bookings_path(:r_id => params[:r_id]) # take back to index page, which now list the newly created user also
+    rescue Exception => e
+     flash[:error] = "Booking Failed to save"
+     render :new
     end
   end
 
