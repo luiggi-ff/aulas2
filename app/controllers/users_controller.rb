19590f78d4341cb4dbf04ee3f0aa7cdc12139f99
Luiggi Franceschi <luiggi_ff@yahoo.com.ar>
2014-11-18 23:41:16 -0300

    admin added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-
+  before_filter :ensure_admin
+        
+  def ensure_admin
+     redirect_to resources_path unless current_user.admin?
+  end
 
   def index
      @users = User.all
